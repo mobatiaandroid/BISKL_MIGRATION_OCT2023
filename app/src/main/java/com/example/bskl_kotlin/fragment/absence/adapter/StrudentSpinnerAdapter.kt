a8a980f440866df8bd83cd61ad6c2141d6bbdfee
mtplmacminio2 <sajna.ar@mobatia.com>
2023-06-05 12:25:06 +0530

    absence adapter

---
+++ app/src/main/java/com/example/bskl_kotlin/fragment/absence/adapter/StrudentSpinnerAdapter.kt
@@ -0,0 +1,73 @@
+package com.example.bskl_kotlin.fragment.absence.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.bskl_kotlin.fragment.absence.model.StudentModel
+import com.mobatia.bskl.R
+
+class StrudentSpinnerAdapter(private val mContext: Context, mStudentList: ArrayList<StudentModel>) :
+    RecyclerView.Adapter<StrudentSpinnerAdapter.MyViewHolder>() {
+    private val mStudentList: ArrayList<StudentModel>
+    var dept: String? = null
+
+    inner class MyViewHolder(view: View) : ViewHolder(view) {
+        var mTitleTxt: TextView
+        var listTxtClass: TextView
+        var imgIcon: ImageView
+        var relSub: RelativeLayout
+
+        init {
+            mTitleTxt = view.findViewById(R.id.listTxtTitle)
+            listTxtClass = view.findViewById(R.id.listTxtClass)
+            imgIcon = view.findViewById(R.id.imagicon)
+            relSub = view.findViewById(R.id.relSub)
+        }
+    }
+
+    init {
+        this.mStudentList = mStudentList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_student_list_adapter, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        if (mStudentList[position].alumini.equals("0")) {
+            holder.relSub.visibility = View.VISIBLE
+            holder.mTitleTxt.setText(mStudentList[position].mName)
+            holder.imgIcon.visibility = View.VISIBLE
+            holder.listTxtClass.setText(mStudentList[position].mClass)
+            if (!mStudentList[position].mPhoto.equals("")) {
+                /*Picasso.with(mContext)
+                    .load(AppUtils.replace(mStudentList[position].mPhoto.toString()))
+                    .placeholder(R.drawable.boy).fit().into(holder.imgIcon)*/
+            } else {
+                holder.imgIcon.setImageResource(R.drawable.boy)
+            }
+        } else {
+            holder.relSub.visibility = View.GONE
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mStudentList.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+}
---
+++ app/src/main/java/com/example/bskl_kotlin/fragment/absence/model/StudentModel.kt
@@ -0,0 +1,28 @@
+package com.example.bskl_kotlin.fragment.absence.model
+
+class StudentModel (
+    var abscenceId //attendance Id
+    : String? = null,
+    var absenceCodeId: String? = null,
+    var abRegister: String? = null,
+    var isPresent: String? = null,
+    var isLate: String? = null,
+    var date: String? = null,
+    var schoolId: String? = null,
+    var status: String? = null,
+    var parent_id: String? = null,
+    var parent_name: String? = null,
+    var app_updated_on: String? = null,
+    var type: String? = null,
+
+    var mId: String? = null,
+    var mName: String? = null,
+    var mClass: String? = null,
+    var mSection: String? = null,
+    var progressreport: String? = null,
+    var alumini: String? = null,
+    var goingStatus: String? = null,
+    var mPhoto: String? = null,
+    var mHouse: String? = null,
+    var viewType: String? = null
+        )
