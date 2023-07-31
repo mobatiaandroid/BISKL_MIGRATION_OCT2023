2178f6baae8ed89c143f34f4bd07e8748f28a14c
mtplmacminio2 <sajna.ar@mobatia.com>
2023-07-31 12:31:27 +0530

    last commit.

--- app/src/main/java/com/example/bskl_kotlin/common/model/StudentResponseModel.kt
+++ app/src/main/java/com/example/bskl_kotlin/common/model/StudentResponseModel.kt
@@ -8,7 +8,6 @@ import com.example.bskl_kotlin.fragment.absence.model.StudentModel
 import com.google.gson.annotations.SerializedName
 
 class StudentResponseModel (
-
     @SerializedName("response")var response:String,
     @SerializedName("statuscode")var statuscode:String,
     @SerializedName("data")var data:ArrayList<StudentModel>
